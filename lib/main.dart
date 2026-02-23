69ecb7a4dc87556a51ce3c730b3026fca5a0ef7c
Iffat Ara Magduri <iamagduri@gmail.com>
2026-02-22 21:48:55 -0500

    Implemented Login Screen UI and basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/screens/login_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return  MaterialApp(
+      title: 'MediCare',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF00796B),
       ),
-    );
+      home: const LoginScreen(),
+      );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/screens/home_screen.dart'; 
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     backgroundColor: const Color(0xFFF5F7FA),
+       body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'MediCare',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF00796B),
+                  ),
+                ),
+                const SizedBox(height: 50),
+          
+                TextField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress, 
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      filled: true,
+                      fillColor: Colors.white, 
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0), 
+                        borderSide: BorderSide.none, 
+                      ),
+                    ),
+                  ),
+          
+                const SizedBox(height: 20),
+                TextField(
+                    controller: _passwordController,
+                    obscureText: true, 
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 40), 
+
+                SizedBox(
+                  width: double.infinity, 
+                  height: 50, 
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF00796B), 
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0), 
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'LOG IN',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        ),
+      );
+  } 
+}
